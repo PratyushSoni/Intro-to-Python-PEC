71e7bff72627649dd27609cca5522494ff70f5f6
PratyushSoni <96909873+PratyushSoni@users.noreply.github.com>
2022-01-14 21:29:35 +0530

    Add files via upload

---
+++ output_computer_assignment1.py
@@ -0,0 +1,41 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:\Users\HP\python\EE_21104043.py ==================
+Enter first number: 213
+Enter second number: 435
+Enter third number: 98
+The average of three numbers is:  248.66666666666666
+
+
+
+Enter gross income $100000000
+Enter the number of dependents 3
+The tax is $ 19996200.0
+
+
+
+Enter your SID: 21104043
+Enter your name: Pratyush Soni
+Enter your gender(F-female, M-male, U-unknown): M
+Enter your course name: Electrical Engineering
+Enter your CGPA: 9.8
+Student Info [21104043, 'Pratyush Soni', 'M', 'Electrical Engineering', 9.8]
+
+
+
+Enter marks of student 1: 98
+Enter marks of student 2: 90
+Enter marks of student 3: 87
+Enter marks of student 4: 94
+Enter marks of student 5: 96
+[87.0, 90.0, 94.0, 96.0, 98.0]
+
+
+
+Color ['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
+
+
+
+x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
